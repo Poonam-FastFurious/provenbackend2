58db16ed4ddd4d4a0d76cd5853e29e539faebacc
Poonam-FastFurious <Poonam.brandbell@gmail.com>
2025-09-07 15:46:30 +0530

    upadte

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
