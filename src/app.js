4d6dda4241538a35aea2e374c607673baecc7049
Poonam-FastFurious <Poonam.brandbell@gmail.com>
2025-09-07 15:38:44 +0530

    corss

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://provenadmin.brandbell.in",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
